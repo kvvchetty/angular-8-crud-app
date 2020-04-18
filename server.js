475b0e24f9097776a75809c69287c84316d0b9ea
kvvchetty <kvvchetty@outlook.com>
2020-04-18 07:25:04 -0400

    csrf fix

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const csrf = require('csurf');
 
 const app = express();
+
+app.use(cookieParser('secretPassword'));
+app.use(csrf());
+app.use(function(req, res, next) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  return next();
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/Angular8ClientCrud'));
